fb099114fbde99e70c2b81b99c8bd237625b1882
FrancoFlores1101 <103612128+FrancoFlores1101@users.noreply.github.com>
2022-06-01 23:05:53 -0300

    Add files via upload

---
+++ ejercicio 5.py
@@ -0,0 +1,10 @@
+from zope.interface import Interface
+from zope.interface import implementer
+
+class Interface(Interface):
+    def insertarElemento(self):
+        pass
+    def agregarElemento(self):
+        pass
+    def insertarElemento(self):
+        pass
